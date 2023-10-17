146d92e172c6a155c1aa51c091629cd45f5b1678
LuKz2 <lucasoliveiraangelo@hotmail.com>
2023-10-17 18:34:43 -0300

    first commit

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,11 +1,10 @@
 import React from "react"
 import { StyleSheet, Text, Dimensions, TouchableHighlight } from "react-native"
 
-export default props =>{
-    return(
+export default props => {
+    return (
         <TouchableHighlight onPress={props.onClick}>
             <Text style={styles.button}>{props.label}</Text>
-
         </TouchableHighlight>
     )
 }
@@ -13,11 +12,11 @@ export default props =>{
 
 
 const styles = StyleSheet.create({
-    button:{
-        fontSize:40,
+    button: {
+        fontSize: 40,
         height: Dimensions.get('window').width / 4,
         width: Dimensions.get('window').width / 4,
-        padding:20,
+        padding: 20,
         backgroundColor: '#f0f0f0',
         textAlign: "center",
         borderWidth: 1,
