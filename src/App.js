50b5a6406e1bdc8c1adc9cf572650f5525e01811
LuKz2 <lucasoliveiraangelo@hotmail.com>
2023-10-19 15:01:43 -0300

    adicionando as operações

--- src/App.js
+++ src/App.js
@@ -15,29 +15,51 @@ export default class App extends Component {
   state = { ...init }
 
   addDig = n => {
-    if (n === '.' && this.state.displayValue.includes('.')) {
-      return
-    }
+    console.debug(typeof this.state.displayValue)
     const clearDisplay = this.state.displayValue === '0'
       || this.state.clearDisplay
+
+    if (n === '.' && !clearDisplay && this.state.displayValue.includes('.')) {
+      return
+    }
+
     const currentValue = clearDisplay ? '' : this.state.displayValue
     const displayValue = currentValue + n
-      this.setState({ displayValue, clearDisplay: false })
+    this.setState({ displayValue, clearDisplay: false })
 
     if (n !== '.') {
       const newValue = parseFloat(displayValue)
       const values = [...this.state.values]
       values[this.state.current] = newValue
-      this.setState({values})
+      this.setState({ values })
     }
   }
 
   clearMemory = () => {
-   this.setState({...init})
+    this.setState({ ...init })
   }
 
   setOperation = operation => {
+    if (this.state.current === 0) {
+      this.setState({ operation, current: 1, clearDisplay: true })
+    } else {
+      const equals = operation === '='
+      const values = [...this.state.values]
+      try {
+        values[0] = eval(`${values[0]} ${this.state.operation} ${values[1]}`)
+      } catch (e) {
+        values[0] = this.state.values[0]
+      }
 
+      values[1] = 0
+      this.setState({
+        displayValue: `${values[0]}`,
+        operation: equals ? null : operation,
+        current: equals ? 0 : 1,
+        clearDisplay: true,
+        values: values
+      })
+    }
   }
 
   render() {
